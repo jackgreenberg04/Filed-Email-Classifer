f8d266c09f1e044965fc72c13de99d97bea79837
Jack Greenberg <164659759+jackgreenberg04@users.noreply.github.com>
2025-06-07 02:40:12 -0400

    Delete tests directory

--- tests/test_preprocess.py
+++
@@ -1,8 +0,0 @@
-from email_classifier.preprocess import clean_text
-
-
-def test_clean_text_basic():
-    text = "Hello!!! This is a TEST email, visit http://example.com"
-    cleaned = clean_text(text)
-    assert 'http' not in cleaned
-    assert cleaned.islower()
